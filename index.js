d4e20fd51afb64870ccda3ff631180fc96e88048
Maciej Kucmus <maciejkucmus@gmail.com>
2020-03-16 14:28:56 +0100

    debug

--- index.js
+++ index.js
@@ -41,9 +41,7 @@ const upsertDeployComment = async (client, repo, deployUrl, namespace, issue, pu
   const { sha: commitHash, repo, payload, issue} = github.context
 
   const prNumber = payload.pull_request && payload.pull_request.number
-  console.log(payload.pull_request);
-  core.debug(payload);
-
+  console.log(payload);
 
   if (!githubToken || !prNumber || !namespace) {
     core.setFailed('Some action arguments are missing. Action has failed.');
